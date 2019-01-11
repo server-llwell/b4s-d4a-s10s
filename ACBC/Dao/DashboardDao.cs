2b644cf8d879391427de56df63a0a6c5ce47ed82
h123a <40271842@qq.com>
2019-01-11 13:51:13 +0800

    123

--- ACBC/Dao/DashboardDao.cs
+++ ACBC/Dao/DashboardDao.cs
@@ -1325,21 +1325,21 @@ namespace ACBC.Dao
                     {
                         month = dr["month"].ToString().Substring(5),
                         type = "线上店",
-                        value = dr["SUPPLY_AMOUNT1"].ToString()==""?0:Convert.ToDouble( dr["SUPPLY_AMOUNT1"].ToString()),
+                        value = dr["SUPPLY_AMOUNT1"].ToString()==""? 0.1 : Convert.ToDouble( dr["SUPPLY_AMOUNT1"].ToString()),
                     };
                     salesTrendData.salesTrends.Add(salesTrend1);
                     SalesTrend salesTrend2 = new SalesTrend
                     {
                         month = dr["month"].ToString().Substring(5),
                         type = "线下店",
-                        value = dr["SUPPLY_AMOUNT2"].ToString() == "" ? 0 : Convert.ToDouble(dr["SUPPLY_AMOUNT2"].ToString()),
+                        value = dr["SUPPLY_AMOUNT2"].ToString() == "" ? 0.1 : Convert.ToDouble(dr["SUPPLY_AMOUNT2"].ToString()),
                     };
                     salesTrendData.salesTrends.Add(salesTrend2);
                     SalesTrend salesTrend3 = new SalesTrend
                     {
                         month = dr["month"].ToString().Substring(5),
                         type = "一般贸易",
-                        value = dr["SUPPLY_AMOUNT3"].ToString() == "" ? 0 : Convert.ToDouble(dr["SUPPLY_AMOUNT3"].ToString()),
+                        value = dr["SUPPLY_AMOUNT3"].ToString() == "" ? 0.1 : Convert.ToDouble(dr["SUPPLY_AMOUNT3"].ToString()),
                     };
                     salesTrendData.salesTrends.Add(salesTrend3);
                 }
@@ -1585,14 +1585,14 @@ namespace ACBC.Dao
                     {
                         month = dr["month"].ToString().Substring(5),
                         type = "销售金额",
-                        value = dr["totalprice"].ToString() == "" ? 0 :Convert.ToDouble( dr["totalprice"].ToString()),
+                        value = dr["totalprice"].ToString() == "" ? 0.1 : Convert.ToDouble( dr["totalprice"].ToString()),
                     };
                     salesTrendData.salesTrends.Add(salesTrend1);
                     SalesTrend salesTrend2 = new SalesTrend
                     {
                         month = dr["month"].ToString().Substring(5),
                         type = "平台利润",
-                        value = dr["platformprice"].ToString() == "" ? 0 : Convert.ToDouble(dr["totalprice"].ToString()),
+                        value = dr["platformprice"].ToString() == "" ? 0.1 : Convert.ToDouble(dr["totalprice"].ToString()),
                     };
                     salesTrendData.salesTrends.Add(salesTrend2);
                 }
@@ -1679,7 +1679,7 @@ namespace ACBC.Dao
                 "GROUP BY DATE_FORMAT(TRADETIME,'%Y-%m-%d') ORDER BY DATE_FORMAT(TRADETIME,'%Y-%m-%d') DESC LIMIT 10";
 
             public const string SELECT_HOMEPAGE_SELLTOTAL = "SELECT SUM(PURCHASEPRICE) PURCHASEPRICE,SUM(PLATFORMPRICE) PLATFORMPRICE FROM V_HOMEPAGE_TOTALSELL";
-            public const string SELECT_HOMEPAGE_THISYEAR_SELL = "SELECT * FROM V_HOMEPAGE_THISYEAR_SELL WHERE MONTH BETWEEN '{0}' AND  '{1}' ORDER BY MONTH DESC";
+            public const string SELECT_HOMEPAGE_THISYEAR_SELL = "SELECT * FROM V_HOMEPAGE_THISYEAR_SELL WHERE MONTH BETWEEN '{0}' AND  '{1}' ORDER BY MONTH ASC";
             public const string SELECT_HOMEPAGE_SELLTOTAL_PROPORTION = "SELECT * FROM V_HOMEPAGE_TOTALSELL_PROPORTION ";
 
             public const string SELECT_SHOPID_BY_TRADE = "SELECT U.USERCODE,U.USERNAME FROM T_PURCHASE_LIST P ,T_USER_LIST U WHERE P.USERCODE = U.USERCODE AND P.USERCODE <>'ADMIN' GROUP BY U.USERCODE,U.USERNAME ";
